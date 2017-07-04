37ec5e783f9dbc0a746f89b246b8a02876357453
dariosus <dario@digitalhouse.com>
2017-07-04 09:54:05 -0300

    migration update

---
+++ database/migrations/2017_07_04_124705_alter_table_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('price');
+            $table->string('slug', 255)->change();
+            $table->renameColumn('name', 'title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
